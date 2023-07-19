3dde20787b8e0ae188e6f1277c602b1a8dfedc20
Fer Illanes <illanes.f.m@gmail.com>
2023-07-18 22:23:17 -0300

    prueba

--- script/script.js
+++ script/script.js
@@ -104,23 +104,22 @@ function miPrograma() {
   }
 
   function renderizarPersonajes(array, contenedor, jugador, titulo) {
+    console.log(array)
     contenedor.innerHTML = ""
-
     array.forEach(personaje => {
       let tarjetaPersonaje = renderizar(personaje, contenedor)
       tarjetaPersonaje.addEventListener("click", () => {
-        jugador === "uno"
-          ? (sessionStorage.setItem("jugadorUno", JSON.stringify(personaje)),
-            contenedor.removeChild(tarjetaPersonaje),
-            console.log(array),
-            nuevasTarjetas = array.filter(p => p !== personaje),
-            console.log(nuevasTarjetas),
-            ocultar(contenedor),
-            ocultar(titulo),
-            seleccionJugadorDos(nuevasTarjetas),
-            console.log(nuevasTarjetas))
-          : (sessionStorage.setItem("jugadorDos", JSON.stringify(personaje)),
-            batalla(JSON.parse(sessionStorage.getItem("jugadorUno")), personaje))
+        if (jugador === "uno") {
+          sessionStorage.setItem("jugadorUno", JSON.stringify(personaje))
+          contenedor.removeChild(tarjetaPersonaje);
+          let nuevasTarjetas = array.filter(p => p !== personaje)
+          ocultar(contenedor)
+          ocultar(titulo)
+          seleccionJugadorDos(nuevasTarjetas)
+        } else {
+          sessionStorage.setItem("jugadorDos", JSON.stringify(personaje))
+          batalla(JSON.parse(sessionStorage.getItem("jugadorUno")), personaje)
+        }
       })
     })
   }
@@ -142,6 +141,7 @@ function miPrograma() {
     inputJugadorUno.addEventListener("input", () => {
       const filtro = inputJugadorUno.value
       const personajesFiltrados = filtrarPersonajes(array, filtro)
+      console.log(personajesFiltrados)
       renderizarPersonajes(personajesFiltrados, contenedorTarjetas, "uno", titulo)
     })
 
@@ -149,24 +149,23 @@ function miPrograma() {
   }
 
   function seleccionJugadorDos(array) {
-    console.log(array)
     let titulo = document.createElement("div")
     titulo.classList.add("titulo")
     titulo.innerHTML = `<h2> Jugador dos, elige tu campeón </h2><input id="filtroJugadorDos" placeholder="Filtra por clase/raza">`
 
-    let contenedorTarjetasDos = document.createElement("div")
-    contenedorTarjetasDos.classList.add("contenedorTarjetasDos")
+    let contenedorTarjetas = document.createElement("div")
+    contenedorTarjetas.classList.add("contenedorTarjetas")
 
-    contenedorSeleccion.append(titulo, contenedorTarjetasDos)
+    contenedorSeleccion.append(titulo, contenedorTarjetas)
 
     let inputJugadorDos = document.getElementById("filtroJugadorDos")
     inputJugadorDos.addEventListener("input", () => {
       const filtroDos = inputJugadorDos.value
       const personajesFiltradosDos = filtrarPersonajes(array, filtroDos)
-      renderizarPersonajes(personajesFiltradosDos, contenedorTarjetasDos, "dos", titulo)
+      renderizarPersonajes(personajesFiltradosDos, contenedorTarjetas, "dos", titulo)
     })
 
-    renderizarPersonajes(array, contenedorTarjetasDos, "dos", titulo)
+    renderizarPersonajes(array, contenedorTarjetas, "dos", titulo)
   }
 
   function batalla(jugadorUno, jugadorDos) {
